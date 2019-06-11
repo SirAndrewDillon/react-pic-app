ee657fd851b7e15510e57790ce513a843a5a730f
Andrew Dillon <majik895@gmail.com>
2019-06-11 18:13:10 -0400

    Scaffolded project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//Components
+import SearchBar from './SearchBar'
+
+const App = () => {
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    )
+}
+
+
+
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
