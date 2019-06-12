17c2ababad15fccb27e73fc52028826da5f87fa8
Andrew Dillon <majik895@gmail.com>
2019-06-12 10:26:18 -0400

    Added preventDefault

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 
 
 
 
 class SearchBar extends Component {
+        state = { term: '' };
+
+        onFormSubmit = (event) => {
+            event.preventDefault();
+        }
+    
+
+
     render() {
         return (
             <div className = 'ui segment'>
-                <form className = 'ui form'>
+                <form onSubmit = {this.onFormSubmit} className = 'ui form'>
                     <div className = 'field'> 
-                    <label htmlFor="Image Search">Image Search</label>
-                    <input type="text"/>
+                    <label>Image Search</label>
+                    <input 
+                    type="text" 
+                    value={this.state.term} 
+                    onChange = {(e) => this.setState({ term: e.target.value })} 
+                    />
                     </div>
                 </form>
             </div>
-        );
+        )
     }
 }
 
+
 export default SearchBar;
+
+
+
+
